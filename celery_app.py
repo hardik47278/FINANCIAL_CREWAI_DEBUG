3a3005b5f28df60704d2425af6d48e90e8614b37
HARDIK ANAND <hardikanand47h@gmail.com>
2026-04-15 11:14:01 +0530

    Update celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -5,7 +5,7 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 celery_app = Celery(
     "financial_analyzer",
-    broker=REDIS_URL,
-    backend=REDIS_URL,
-    include=["job"],   # ✅ ADD THIS
+    broker=REDIS_URL,#celery sends task to redis  ## fastapi - send task->redis quene ->celery worker picks up
+    backend=REDIS_URL,#stores task
+    include=["job"],   # load from job.py 
 )
